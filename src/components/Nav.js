ebbe27b90e0845b911def537f9949d49e9c25701
Erin <erinllouise3@gmail.com>
2022-01-19 11:21:03 +1300

    setup global styles and nav component responsive

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,26 @@
 import React from 'react'
+//local files css
+import '../styles/nav.css'
+//global file css
+import '../styles.css'
 
 function Nav () {
     return (
-        <div>
-            
+        <div className='html'>
+            <div className='body'>
+                <div className='wrapper'>
+                    <nav className='flex-nav'>
+                        <ul>
+                        <li><a href="#">Item 01</a></li>
+                        <li><a href="#">Item 02</a></li>
+                        <li><a href="#">Item 03</a></li>
+                        <li><a href="#">Item 04</a></li>
+                        <li><a href="#">Item 05</a></li>
+                        <li><a href="#">Item 06</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
     )
 }
