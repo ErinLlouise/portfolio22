b9170221deeeb1efeae89f48a418dc53c7d69df2
Erin <erinllouise3@gmail.com>
2022-01-19 12:37:01 +1300

    add home scroll animation not working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../styles.css'
 import '../styles/home.css'
+import HomeAnim from './animations/HomeAnim'
 function Home () {
     return (
         <div className='html'>
             <div className='body home'>
                 <div className='wrapper'>
                     <h1>ERIN LOUISE</h1>
-                    <h1>DEVELOPER/DESIGNER</h1>
+                    <HomeAnim />
                 </div>
             </div>
         </div>
---
+++ src/components/animations/HomeAnim.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+import '../../styles/homeanim.css'
+
+function HomeAnim () {
+    return (
+        <div className='body'>
+            <div className='bar'>
+                <span className='bar_content'>DEVELOPER/DESIGNER/CREATIVE TECHNOLOGIST/LOREMIPSUM/DEVELOPER/DESIGNER/CREATIVE TECHNOLOGIST/LOREMIPSUM/</span>
+            </div>
+        </div>
+    )
+}
+
+export default HomeAnim
