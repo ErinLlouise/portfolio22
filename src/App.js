ccb8a2eaa427453519e2c7e2de505b6afb7a3f9e
Erin <erinllouise3@gmail.com>
2022-01-19 15:13:30 +1300

    add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router'
+import About from './components/About';
+import Contact from './components/Contact';
 import Home from './components/Home';
-// import Nav from './components/Nav';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Nav /> */}
       <Home />
+      <Routes>
+        <Route path='/home' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/projects' component={Projects} />
+        <Route path='/skills' component={Skills} />
+        <Route path='/contact' component={Contact} />
+      </Routes>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,11 +11,11 @@ function Nav () {
                 <div className='wrapper'>
                     <nav className='flex-nav'>
                         <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Skills</a></li>
-                            <li><a href="#">Contact</a></li>
+                            <li><a href="/home">Home</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/projects">Projects</a></li>
+                            <li><a href="skills">Skills</a></li>
+                            <li><a href="/contact">Contact</a></li>
                         </ul>
                     </nav>
                 </div>
